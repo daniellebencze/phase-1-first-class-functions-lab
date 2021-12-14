13c215754dff621637e1127dab08a55f3e39d17e
Danielle Bencze <daniellebencze@live.com>
2021-12-14 08:48:33 -0800

    commit tree 2c084fba4b72704e8f2e73547478a56248fab07b

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer) {
+    return function(fareMultiplier) {
+        return (integer * fareMultiplier);
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+
+const selectDifferentDrivers = function(drivers, arrayOfDrivers) {
+    return arrayOfDrivers(drivers)
+}
